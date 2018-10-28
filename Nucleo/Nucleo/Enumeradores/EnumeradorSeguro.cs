ea66a4f2489d62cb83401530c3c7e0abfc592e96
Gabriel Cotrim <gabrielcotrimdev@gmail.com>
2018-10-28 11:35:13 -0300

    Refatora classe EnumeradorSeguro

---
+++ Nucleo/Nucleo/Enumeradores/EnumeradorSeguro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleo.Enumeradores
+{
+    public class EnumeradorSeguro<T, k> : IEnumeradorSeguro<k> where T : IEnumeradorSeguro<k>
+    {
+        public k Codigo<k>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
