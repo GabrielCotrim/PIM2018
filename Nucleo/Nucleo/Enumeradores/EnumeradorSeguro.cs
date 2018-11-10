b3ef520048228dd9a68d8af6e6470f6f1acdecbb
Gabriel Cotrim <gabrielcotrim@outlook.com>
2018-11-10 17:07:00 -0200

    Implementa a classe Enumerador, fim

--- Nucleo/Nucleo/Enumeradores/EnumeradorSeguro.cs
+++ Nucleo/Nucleo/Enumeradores/EnumeradorSeguro.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Nucleo.Enumeradores
 {
-    public class EnumeradorSeguro<T, k> : IEnumeradorSeguro<k>, IComparable where T : IEnumeradorSeguro<k>
+    public class EnumeradorSeguro<T, k> : IEnumeradorSeguro<k> where T : IEnumeradorSeguro<k>
     {
+        public k Codigo => _codigo;
+        public string Descricao => _descricao;
+
         private k _codigo;
         private string _descricao;
 
@@ -17,21 +21,38 @@ namespace Nucleo.Enumeradores
             _descricao = descricao;
         }
 
-        public IEnumerable<T> ObtenhaTodos()
+        public List<T> ObtenhaTodos()
         {
-            throw new NotImplementedException();
+            return ObtenhaTodos<T>();
         }
 
-        public k Codigo => _codigo;
-        public string Descricao => _descricao;
-
-        public int CompareTo(object other)
+        public override bool Equals(object outroObjeto)
         {
-            if (other as IEnumeradorSeguro<k> == null) return 1;
-
-            var otherEnumerador = other as IEnumeradorSeguro<k>;
-
-            return CompareTo(otherEnumerador);
+            return outroObjeto is EnumeradorSeguro<T,k> && ((EnumeradorSeguro<T, k>)outroObjeto).Codigo.Equals(Codigo);
         }
+        
+        public T ObtenhaEnumerador(k codigo)
+        {
+            return ObtenhaEnumerador<T>(codigo);
+        }
+
+        protected TEnumerador ObtenhaEnumerador<TEnumerador>(k codigo) where TEnumerador : IEnumeradorSeguro<k>
+        {
+            foreach(var enumerador in ObtenhaTodos<TEnumerador>())
+            {
+                if (enumerador.Codigo.Equals(codigo))
+                {
+                    return enumerador;
+                }
+            }
+            return default(TEnumerador);
+        }
+
+        protected List<TEnumerador> ObtenhaTodos<TEnumerador>() where TEnumerador : IEnumeradorSeguro<k>
+        {
+            var tipo = typeof(TEnumerador);
+            return tipo.GetFields(BindingFlags.Static | BindingFlags.Public).Select(campo => (TEnumerador)campo.GetValue(null)).ToList();
+        }
+
     }
 }
