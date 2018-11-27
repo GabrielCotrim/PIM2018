a9fb37e6f1afcc9c13c5deefdf4dd53e6c4dcd98
Gabriel Cotrim <gabrielcotrim@outlook.com>
2018-11-27 16:16:58 -0200

    Add class enumerador;

---
+++ Nucleo/Nucleo/Enumeradores/EnumeradorGravidadeDaTarefa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleo.Enumeradores
+{
+    public sealed class EnumeradorGravidadeDaTarefa : EnumeradorSeguro<EnumeradorGravidadeDaTarefa, int>
+    {
+        public static EnumeradorGravidadeDaTarefa PEQUENO = new EnumeradorGravidadeDaTarefa(1, "Pequeno");
+        public static EnumeradorGravidadeDaTarefa GRANDE = new EnumeradorGravidadeDaTarefa(2, "Grande");
+        public static EnumeradorGravidadeDaTarefa TRAVAMENTO = new EnumeradorGravidadeDaTarefa(3, "Travamento");
+
+        private EnumeradorGravidadeDaTarefa(int cod, string descricao) : base(cod, descricao)
+        {
+
+        }
+    }
+}
