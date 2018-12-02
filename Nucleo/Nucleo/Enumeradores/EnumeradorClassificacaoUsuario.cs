21610c2e43aacb7300c903c4c100c093867c2243
Gabriel Cotrim <gabrielcotrim@outlook.com>
2018-12-02 13:47:45 -0200

    Adiciona Enumerador para classificar usuarios;

---
+++ Nucleo/Nucleo/Enumeradores/EnumeradorClassificacaoUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleo.Enumeradores
+{
+    public sealed class EnumeradorClassificacaoUsuario : EnumeradorSeguro<EnumeradorClassificacaoUsuario, int>
+    {
+        public static EnumeradorClassificacaoUsuario ADMINISTRADOR = new EnumeradorClassificacaoUsuario(1, "Administrador");
+        public static EnumeradorClassificacaoUsuario SUPORTE = new EnumeradorClassificacaoUsuario(2, "Suporte");
+        public static EnumeradorClassificacaoUsuario DESENVOLVEVIMENTO = new EnumeradorClassificacaoUsuario(3, "Desenvolvimento");
+        public static EnumeradorClassificacaoUsuario PROJETO = new EnumeradorClassificacaoUsuario(4, "Projeto");
+
+        private EnumeradorClassificacaoUsuario(int cod, string descricao) : base(cod, descricao)
+        {
+
+        }
+    }
+}
