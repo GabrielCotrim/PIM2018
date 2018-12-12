e028040e9f2f123a6ee3d21acc634c253524b1ea
Gabriel Cotrim <gabrielcotrim@outlook.com>
2018-12-12 02:00:28 -0200

    Refatoração;

--- Nucleo/InterfacesVisuais/frmCadastroTarefa.Designer.cs
+++ Nucleo/InterfacesVisuais/frmCadastroTarefa.Designer.cs
@@ -36,15 +36,24 @@
             this.lblEstado = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.cboGravidade = new System.Windows.Forms.ComboBox();
+            this.cboEstado = new System.Windows.Forms.ComboBox();
+            this.cboAtribuido = new System.Windows.Forms.ComboBox();
+            this.cboPrioridade = new System.Windows.Forms.ComboBox();
+            this.cboRelator = new System.Windows.Forms.ComboBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnOlhar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblVisualizar = new System.Windows.Forms.Label();
+            this.dgvTarefas = new System.Windows.Forms.DataGridView();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTarefas)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -147,12 +156,12 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.22017F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.18182F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.31382F));
-            this.tableLayoutPanel2.Controls.Add(this.comboBox6, 5, 0);
-            this.tableLayoutPanel2.Controls.Add(this.comboBox5, 4, 0);
-            this.tableLayoutPanel2.Controls.Add(this.comboBox4, 3, 0);
-            this.tableLayoutPanel2.Controls.Add(this.comboBox3, 2, 0);
-            this.tableLayoutPanel2.Controls.Add(this.comboBox2, 1, 0);
-            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cboRelator, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cboPrioridade, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cboAtribuido, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cboEstado, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cboGravidade, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cboCategoria, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 38);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
@@ -161,72 +170,135 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(813, 28);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
-            // comboBox1
+            // cboCategoria
             // 
-            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 3);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(135, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cboCategoria.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Location = new System.Drawing.Point(3, 3);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(135, 21);
+            this.cboCategoria.TabIndex = 0;
             // 
-            // comboBox2
+            // cboGravidade
             // 
-            this.comboBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(144, 3);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(141, 21);
-            this.comboBox2.TabIndex = 1;
+            this.cboGravidade.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboGravidade.FormattingEnabled = true;
+            this.cboGravidade.Location = new System.Drawing.Point(144, 3);
+            this.cboGravidade.Name = "cboGravidade";
+            this.cboGravidade.Size = new System.Drawing.Size(141, 21);
+            this.cboGravidade.TabIndex = 1;
             // 
-            // comboBox3
+            // cboEstado
             // 
-            this.comboBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(291, 3);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(97, 21);
-            this.comboBox3.TabIndex = 2;
+            this.cboEstado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboEstado.FormattingEnabled = true;
+            this.cboEstado.Location = new System.Drawing.Point(291, 3);
+            this.cboEstado.Name = "cboEstado";
+            this.cboEstado.Size = new System.Drawing.Size(97, 21);
+            this.cboEstado.TabIndex = 2;
             // 
-            // comboBox4
+            // cboAtribuido
             // 
-            this.comboBox4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(394, 3);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(134, 21);
-            this.comboBox4.TabIndex = 3;
+            this.cboAtribuido.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboAtribuido.FormattingEnabled = true;
+            this.cboAtribuido.Location = new System.Drawing.Point(394, 3);
+            this.cboAtribuido.Name = "cboAtribuido";
+            this.cboAtribuido.Size = new System.Drawing.Size(134, 21);
+            this.cboAtribuido.TabIndex = 3;
             // 
-            // comboBox5
+            // cboPrioridade
             // 
-            this.comboBox5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(534, 3);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(141, 21);
-            this.comboBox5.TabIndex = 4;
+            this.cboPrioridade.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboPrioridade.FormattingEnabled = true;
+            this.cboPrioridade.Location = new System.Drawing.Point(534, 3);
+            this.cboPrioridade.Name = "cboPrioridade";
+            this.cboPrioridade.Size = new System.Drawing.Size(141, 21);
+            this.cboPrioridade.TabIndex = 4;
             // 
-            // comboBox6
+            // cboRelator
             // 
-            this.comboBox6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(681, 3);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(129, 21);
-            this.comboBox6.TabIndex = 5;
+            this.cboRelator.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboRelator.FormattingEnabled = true;
+            this.cboRelator.Location = new System.Drawing.Point(681, 3);
+            this.cboRelator.Name = "cboRelator";
+            this.cboRelator.Size = new System.Drawing.Size(129, 21);
+            this.cboRelator.TabIndex = 5;
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(715, 150);
+            this.flowLayoutPanel1.Controls.Add(this.btnOlhar);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(733, 66);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(95, 100);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(80, 410);
             this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // btnOlhar
+            // 
+            this.btnOlhar.Location = new System.Drawing.Point(3, 3);
+            this.btnOlhar.Name = "btnOlhar";
+            this.btnOlhar.Size = new System.Drawing.Size(75, 38);
+            this.btnOlhar.TabIndex = 0;
+            this.btnOlhar.Text = "Visualizar";
+            this.btnOlhar.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.dgvTarefas, 0, 2);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 66);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 37.87879F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 62.12121F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 337F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(733, 410);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.lblVisualizar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(727, 39);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblVisualizar
+            // 
+            this.lblVisualizar.AutoSize = true;
+            this.lblVisualizar.Font = new System.Drawing.Font("Segoe UI Symbol", 20F);
+            this.lblVisualizar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblVisualizar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblVisualizar.Location = new System.Drawing.Point(297, 1);
+            this.lblVisualizar.Name = "lblVisualizar";
+            this.lblVisualizar.Size = new System.Drawing.Size(254, 37);
+            this.lblVisualizar.TabIndex = 1;
+            this.lblVisualizar.Text = "Visualizando tarefas";
+            this.lblVisualizar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dgvTarefas
+            // 
+            this.dgvTarefas.AllowUserToAddRows = false;
+            this.dgvTarefas.AllowUserToDeleteRows = false;
+            this.dgvTarefas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTarefas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvTarefas.Location = new System.Drawing.Point(3, 75);
+            this.dgvTarefas.Name = "dgvTarefas";
+            this.dgvTarefas.ReadOnly = true;
+            this.dgvTarefas.Size = new System.Drawing.Size(727, 332);
+            this.dgvTarefas.TabIndex = 1;
             // 
             // frmCadastroTarefa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(813, 476);
+            this.Controls.Add(this.tableLayoutPanel3);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -236,6 +308,11 @@
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTarefas)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -250,12 +327,17 @@
         private System.Windows.Forms.Label lblAtribuido;
         private System.Windows.Forms.Label lblEstado;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cboRelator;
+        private System.Windows.Forms.ComboBox cboPrioridade;
+        private System.Windows.Forms.ComboBox cboAtribuido;
+        private System.Windows.Forms.ComboBox cboEstado;
+        private System.Windows.Forms.ComboBox cboGravidade;
+        private System.Windows.Forms.ComboBox cboCategoria;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnOlhar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblVisualizar;
+        private System.Windows.Forms.DataGridView dgvTarefas;
     }
 }
