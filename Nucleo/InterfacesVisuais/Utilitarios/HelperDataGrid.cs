776d56fe97592ab332bf06bd4e7c94b4e9652f08
Gabriel Cotrim <gabrielcotrim@outlook.com>
2018-12-12 15:44:55 -0200

    Implementa Ajudador de dgv;

--- Nucleo/InterfacesVisuais/Utilitarios/HelperDataGrid.cs
+++ Nucleo/InterfacesVisuais/Utilitarios/HelperDataGrid.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Nucleo.Utilitarios
 {
     public class HelperDataGrid
     {
-        public static DataGridViewColumn
+
+        public DataGridView DataGrid { get; private set; }
+
+        public HelperDataGrid(DataGridView dataGrid)
+        {
+            DataGrid = dataGrid;
+            FormateDataGrid();
+        }
+
+        public void FormateDataGrid()
+        {
+            DataGrid.AllowUserToAddRows = false;
+            DataGrid.AllowUserToDeleteRows = false;
+            DataGrid.AllowUserToResizeColumns = true;
+            DataGrid.AllowUserToResizeRows = false;
+            DataGrid.AutoGenerateColumns = false;
+            DataGrid.BorderStyle = BorderStyle.None;
+            DataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            DataGrid.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            DataGrid.Margin = new Padding(0);
+            DataGrid.ReadOnly = true;
+            DataGrid.RowHeadersVisible = false;
+            DataGrid.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            DataGrid.ScrollBars = ScrollBars.Vertical;
+            DataGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        public DataGridViewColumn CrieColuna(string headerText ,string nomeDaPropriedade)
+        {
+            var coluna = new DataGridViewTextBoxColumn();
+            coluna.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            coluna.DataPropertyName = nomeDaPropriedade;
+            coluna.HeaderText = headerText;
+            coluna.Name = nomeDaPropriedade;
+            DataGrid.Columns.Add(coluna);
+            return coluna;
+        }
     }
 }
